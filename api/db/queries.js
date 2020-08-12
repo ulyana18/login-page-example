3dc8a586ddcf1c338d2296c6843c45a864850abe
Ulyana Bychkovskaya <ubychkovskaya20@gmail.com>
2020-08-13 02:33:14 +0300

    feat: add post login

--- api/db/queries.js
+++ api/db/queries.js
@@ -5,7 +5,8 @@ const Pool = require('pg').Pool;
 // const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 const pool = new Pool({
-  user: 'ulyana',
+  // user: 'ulyana',
+  user: 'postgres',
   host: 'localhost',
   database: 'api',
   password: '1234',
--- api/routes/users.js
+++ api/routes/users.js
@@ -3,16 +3,30 @@ var AuthService = require('../services/authservice');
 module.exports = (app) => {
 
   app.post('/user/signup', async (req, res) => {
-    console.log(req.body);
     const name = req.body.user.name;
     const email = req.body.user.email;
     const password = req.body.user.password;
     try {
       const authServiceInstance = new AuthService();
-      const { user, token } = await authServiceInstance.SignUp(name, email, password);
-      // return res.status(200).json({ user, token }).end();
+      const { user, token } = await authServiceInstance.SignUp(name, email, password); 
+
+      return res.send({ user, token }).status(200).end();
     } catch(e) {
-      return res.json(e).status(500).end();
+      return res.send('error').status(401).end();
     }
   })
+
+  app.post('/user/login', async (req, res) => {
+    const email = req.body.user.email;
+    const password = req.body.user.password;
+    try {
+      const authServiceInstance = new AuthService();
+      const { user, token } = await authServiceInstance.LogIn(email, password); 
+
+      return res.send({ user, token }).status(200).end();
+    } catch(e) {
+      return res.send('error').status(402).end();
+    }
+  })
+
 };
